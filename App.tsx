88504cad241604f7f36d32177758d2550d9bed9a
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-11-27 09:28:19 -0600

    add starship screen

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import { Provider as PaperProvider } from "react-native-paper";
 
 import LoginScreen from "./src/screens/LoginScreen";
 import { TermsScreen } from "./src/screens/TermsScreen";
+import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
 
 function App() {
   return (
     <PaperProvider>
       {/* <LoginScreen /> */}
-      <TermsScreen />
+      {/* <TermsScreen /> */}
+      <StarshipFeedScreen />
     </PaperProvider>
   );
 }
