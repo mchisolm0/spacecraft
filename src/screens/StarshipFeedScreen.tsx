5db20b823ec0e76d5a90d26afdcd5dd2e165f598
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2024-11-27 09:27:51 -0600

    create StarshipFeedScreen

---
+++ src/screens/StarshipFeedScreen.tsx
@@ -0,0 +1,27 @@
+import { StatusBar, StyleSheet, Text, View, FlatList } from "react-native";
+
+import { default as data } from "../../api/data.json";
+import StarshipItem from "../components/StarshipItem";
+
+export const StarshipFeedScreen = () => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data.results}
+        keyExtractor={(item) => item.url}
+        renderItem={({ item }) => <StarshipItem starship={item} />}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0, // only for Android to avoid status bar overlap
+  },
+  headerContainer: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+});
