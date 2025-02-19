a335e9b218c63b160ea6165d94f396cb358f0658
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-02-18 22:50:44 -0600

    Remove unused interface

--- src/components/StarshipList.tsx
+++ src/components/StarshipList.tsx
@@ -5,22 +5,6 @@ import { StarshipItem } from "./StarshipItem";
 
 import { useStarships } from "@/hooks/useStarships";
 
-interface Starship {
-  name: string;
-  model: string;
-  manufacturer: string;
-  cost_in_credits: string;
-  length: string;
-  max_atmosphering_speed: string;
-  crew: string;
-  passengers: string;
-  cargo_capacity: string;
-  consumables: string;
-  hyperdrive_rating: string;
-  MGLT: string;
-  starship_class: string;
-}
-
 /**
  * Renders a FlatList of starships using data from the SWAPI API
  */
