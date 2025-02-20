14c7941948b4ab055eeed24d39a050179a6e8274
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-02-20 11:38:58 -0600

    Init storybook

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Alert, View } from "react-native";
+import { Button } from "react-native-paper";
+
+export default {
+  title: "Button",
+};
+
+export const _Button = () => (
+  <View style={{ padding: 16, justifyContent: "space-between", flex: 1 }}>
+    <Button
+      mode="text"
+      onPress={() => Alert.alert("Pressed!")}
+    >
+      Text
+    </Button>
+  </View>
+);
