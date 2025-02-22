bc134f003ad4b16bcb9108f1904c47ea4659e74b
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-02-21 20:44:23 -0600

    Set conditionally using storybook based on env variable

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,3 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const withStorybook = require("@storybook/react-native/metro/withStorybook");
@@ -6,6 +5,8 @@ const withStorybook = require("@storybook/react-native/metro/withStorybook");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 config.transformer.unstable_allowRequireContext = true;
+
+const storyEnabled = process.env.STORYBOOK_ENABLED ?? false;
 
 // Only include storybook files in the metro bundler if storybook is enabled
 config.resolver.resolveRequest = (context, moduleName, platform) => {
@@ -16,7 +17,7 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
   );
 
   if (
-    process.env.STORYBOOK_ENABLED !== "true" &&
+    storyEnabled !== "true" &&
     defaultResolveResult?.filePath?.includes?.(".storybook/")
   ) {
     return {
@@ -30,7 +31,7 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
 module.exports = withStorybook(config, {
   // Set to false to remove storybook specific options
   // you can also use a env variable to set this
-  enabled: true,
+  enabled: storyEnabled,
   // Path to your storybook config
   configPath: path.resolve(__dirname, "./.storybook"),
 
