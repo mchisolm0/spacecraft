cca1b761071fb3029c4bdfc82b96b854d09a43e0
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-03-01 10:08:34 -0600

    Add navigation

--- App.tsx
+++ App.tsx
@@ -4,12 +4,28 @@ import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Constants from "expo-constants";
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { StarshipFeedScreen } from "./src/screens/StarshipFeedScreen";
 
 import { NetworkProvider } from "@/contexts/Network";
 import { LoginScreen } from "@/screens/LoginScreen";
 import { TermsScreen } from "@/screens/TermsScreen";
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "Home",
+  screenOptions: {
+    headerShown: false,
+  },
+  screens: {
+    Home: LoginScreen,
+    Terms: TermsScreen,
+    Starships: StarshipFeedScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 function App() {
   if (__DEV__) {
@@ -24,9 +40,7 @@ function App() {
         <NetworkProvider>
           <SafeAreaProvider>
             <SafeAreaView style={{ flex: 1 }}>
-              <LoginScreen />
-              {/* <TermsScreen /> */}
-              {/* <StarshipFeedScreen /> */}
+              <Navigation />
             </SafeAreaView>
           </SafeAreaProvider>
         </NetworkProvider>
