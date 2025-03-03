d978bfc472827865be187cb4b5c40b9d50da4112
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-03-02 22:08:30 -0600

    Fix detail screen name in routes file

--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -2,5 +2,5 @@ export const Routes = {
   LOGIN_SCREEN: "Login",
   TERMS_SCREEN: "Terms",
   STARSHIPS_SCREEN: "Starships",
-  STARSHIPS_DETAILS_SCREEN: "Details",
+  DETAILS_SCREEN: "Details",
 } as const;
