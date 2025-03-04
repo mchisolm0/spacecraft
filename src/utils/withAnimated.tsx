5e315e9949bef320ccc18d68ade883e3b8cafb71
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-03-03 20:59:50 -0600

    Add withAnimated util for Starship item

---
+++ src/utils/withAnimated.tsx
@@ -0,0 +1,20 @@
+import type { AnimatedProps } from "react-native-reanimated";
+
+import * as React from "react";
+import Animated from "react-native-reanimated";
+
+export function withAnimated<T extends object>(
+  WrappedComponent: React.ComponentType<T>,
+): React.ComponentClass<AnimatedProps<T>, any> {
+  const displayName =
+    WrappedComponent.displayName || WrappedComponent.name || "Componenet";
+
+  class WithAnimated extends React.Component<T, any> {
+    static displayName = `WithAnimated(${displayName})`;
+
+    render(): React.ReactNode {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+  return Animated.createAnimatedComponent(WithAnimated);
+}
