d8affc1cef6c45496f7de90891e37cc304877dc8
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-03-03 20:58:03 -0600

    Add StarshipProps type for api

---
+++ api/types.d.ts
@@ -0,0 +1,20 @@
+export interface StarshipProps {
+  cargo_capacity?: string;
+  consumables?: string;
+  cost_in_credits?: string;
+  created?: string;
+  crew?: string;
+  edited?: string;
+  films?: [];
+  hyperdrive_rating?: string;
+  length?: string;
+  manufacturer?: string;
+  max_atmosphering_speed?: string;
+  MGLT?: string;
+  model: string;
+  name: string;
+  passengers?: string;
+  pilots?: [];
+  starship_class?: string;
+  url?: string;
+}
